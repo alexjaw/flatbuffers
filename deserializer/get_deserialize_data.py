3cebd46b7d5be5e965607a2c879b0832da5dc3e7
Alexander Jaworowski <alexander@jaworowski.se>
2023-04-18 14:20:46 +0000

    Have problems using rust example...

--- deserializer/get_deserialize_data.py
+++ deserializer/get_deserialize_data.py
@@ -1,3 +1,8 @@
+"""
+Test result:
+INFERENCE DATA: 'DAAAAAAABgAKAAQABgAAAAwAAAAAAAYACAAEAAYAAAAEAAAABQAAAEwAAAA0AAAAJAAAABQAAAAEAAAA0P///4sCAAAAAHA93P///18AAAAAAJg96P///5kDAAAAAPg99P///3MCAAAAAPg9CAAMAAQACAAIAAAAUQIAAAAA+D0='
+DESERIALIZED: {'1': {'C': 593, 'P': 0.12109375}, '2': {'C': 627, 'P': 0.12109375}, '3': {'C': 921, 'P': 0.12109375}, '4': {'C': 95, 'P': 0.07421875}, '5': {'C': 651, 'P': 0.05859375}}
+"""
 import base64
 from decode_python import ClassificationTop
 
@@ -29,11 +34,29 @@ def get_deserialize_data_od(serialize_data):
     """
     return buf
 
+def get_deserialize_data_classification_new(serialize_data):
+    """ See https://flatbuffers.dev/flatbuffers_guide_tutorial.html
+    Python
+    """
+    print("--- get_deserialize_data_classification_new()")
+    buf = {}
+    buf_decode = base64.b64decode(serialize_data)
+    detections = ClassificationTop.ClassificationTop.GetRootAs(buf_decode, 0)
+    perception = detections.Perception()
+    #print(dir(perception))
+    #print(dir(perception.ClassificationList(0)))
+    for i in range(perception.ClassificationListLength()):
+        classification = perception.ClassificationList(i)
+        print(f"{i}. C:{classification.ClassId()}, P:{classification.Score()}")
+    print("--- get_deserialize_data_classification_new()")
+    return buf
+
 def get_deserialize_data_classification(serialize_data):
     """Get access information from yaml and generate ConsoleAccess client
     Returns:
         ConsoleAccessClient: CosoleAccessClient Class generated from access information.
     """
+    get_deserialize_data_classification_new(serialize_data)
     buf = {}
     buf_decode = base64.b64decode(serialize_data)
     ppl_out = ClassificationTop.ClassificationTop.GetRootAsClassificationTop(buf_decode, 0)
--- rust_flatbuffers_example/src/main.rs
+++ rust_flatbuffers_example/src/main.rs
@@ -1,10 +1,5 @@
+#![allow(dead_code)]
 extern crate flatbuffers;
-
-mod monster_generated;
-pub use monster_generated::my_game::sample::{
-    Color, Equipment,
-    Monster, MonsterArgs,
-    Vec3, Weapon, WeaponArgs};
 
 mod myschema_generated;
 pub use myschema_generated::users::{
@@ -15,18 +10,65 @@ mod classification_generated;
 pub use classification_generated::dnn_vision::{
     GeneralClassification, GeneralClassificationArgs,
     ClassificationData, ClassificationDataArgs,
-    ClassificationTop, ClassificationTopArgs
+    ClassificationTop, ClassificationTopArgs,
 };
 
-fn monster() {
-    // Build up a serialized buffer algorithmically.
-    // Initialize it with a capacity of 1024 bytes.
-    let mut builder = flatbuffers::FlatBufferBuilder::with_capacity(1024);
+#[allow(dead_code)]
+fn test_file_io() {
+    use std::fs::File;
+    use std::io::prelude::*;
+    use std::path::Path;
 
-    // Serialize some weapons for the Monster: A 'sword' and an 'axe'.
+    let mut path = Path::new("test.bin");
+    let display = path.display();
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut buf = [0u8; 8];
+    match file.write_all(&buf) {
+        Err(why) => panic!("couldn't write to {}: {}", display, why),
+        Ok(_) => println!("successfully wrote to {}", display),        
+    }
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    // Read the file contents into a string, returns `io::Result<usize>`
+    //let mut s = String::new();
+    let mut buf = [0u8, 8];
+    match file.read_exact(&mut buf) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => print!("Ok\n"),
+    }   
+
+}
+
+#[allow(dead_code)]
+fn monster2() {
+}
+
+#[allow(dead_code)]
+fn monster() {
+    // import the generated code
+    #[allow(dead_code, unused_imports)]
+    #[path = "./monster_generated.rs"]
+    mod monster_generated;
+    pub use monster_generated::my_game::sample::{root_as_monster,
+                                                Color, Equipment,
+                                                Monster, MonsterArgs,
+                                                Vec3,
+                                                Weapon, WeaponArgs
+    };
+    let mut builder = flatbuffers::FlatBufferBuilder::new_with_capacity(1024);
+    
     let weapon_one_name = builder.create_string("Sword");
     let weapon_two_name = builder.create_string("Axe");
-
+ 
     // Use the `Weapon::create` shortcut to create Weapons with named field
     // arguments.
     let sword = Weapon::create(&mut builder, &WeaponArgs{
@@ -40,22 +82,22 @@ fn monster() {
 
     // Name of the Monster.
     let name = builder.create_string("Orc");
-
+    
     // Inventory.
-    let inventory = builder.create_vector(&[0u8, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    let inventory = builder.create_vector(&[0u8, 1, 2, 3, 4, 5, 6, 7, 8, 9]);    
 
     // Create a FlatBuffer `vector` that contains offsets to the sword and axe
     // we created above.
     let weapons = builder.create_vector(&[sword, axe]);
 
-    // Create the path vector of Vec3 objects:
-    //let x = Vec3::new(1.0, 2.0, 3.0);
-    //let y = Vec3::new(4.0, 5.0, 6.0);
-    //let path = builder.create_vector(&[x, y]);
-
+    // Create the path vector of Vec3 objects.
+    let x = Vec3::new(1.0, 2.0, 3.0);
+    let y = Vec3::new(4.0, 5.0, 6.0);
+    let path = builder.create_vector(&[x, y]);
+    
     // Note that, for convenience, it is also valid to create a vector of
     // references to structs, like this:
-    // let path = builder.create_vector(&[&x, &y]);
+    // let path = builder.create_vector(&[&x, &y]);    
 
     // Create the monster using the `Monster::create` helper function. This
     // function accepts a `MonsterArgs` struct, which supplies all of the data
@@ -71,37 +113,23 @@ fn monster() {
         weapons: Some(weapons),
         equipped_type: Equipment::Weapon,
         equipped: Some(axe.as_union_value()),
-        //path: Some(path),
+        path: Some(path),
         ..Default::default()
     });
 
-    // Serialize the root of the object, without providing a file identifier.
+    // Call `finish()` to instruct the builder that this monster is complete.
     builder.finish(orc, None);
 
-    // We now have a FlatBuffer we can store on disk or send over a network.
-
-    // ** file/network code goes here :) **
-
-    // Instead, we're going to access it right away (as if we just received it).
     // This must be called after `finish()`.
-    let buf = builder.finished_data(); // Of type `&[u8]`
+    // `finished_data` returns a byte slice.
+    let buf = builder.finished_data(); // Of type `&[u8]`    
     
-    // Get access to the root:
-    let monster = flatbuffers::root::<Monster>(buf).unwrap();
-
-    // Get and test some scalar types from the FlatBuffer.
+    let monster = monster_generated::my_game::
     let hp = monster.hp();
-    let mana = monster.mana();
-    let name = monster.name();
-
-    assert_eq!(hp, 80);
-    assert_eq!(mana, 150);  // default
-    assert_eq!(name, Some("Orc"));    
-
-    println!("The Monsters FlatBuffer was successfully created and accessed!");
-    //dbg!(monster);    
+    dbg!(hp);
 }
 
+#[allow(dead_code)]
 fn user() {
     let mut builder = flatbuffers::FlatBufferBuilder::with_capacity(1024);
     let name = builder.create_string("Arthur Dent");
@@ -121,9 +149,7 @@ fn user() {
     dbg!(arthur);
 }
 
-fn classification() {
-}
-
+#[allow(dead_code)]
 fn test_base64_decoding() {
     //use data_encoding;
     use ascii_converter::*;
@@ -137,9 +163,36 @@ fn test_base64_decoding() {
     };
 }
 
+#[allow(dead_code)]
+fn test_deserialize_classification() {
+    use data_encoding::BASE64;
+    let data = b"DAAAAAAABgAKAAQABgAAAAwAAAAAAAYACAAEAAYAAAAEAAAABQAAAEwAAAA0AAAAJAAAABQAAAAEAAAA0P///4sCAAAAAHA93P///18AAAAAAJg96P///5kDAAAAAPg99P///3MCAAAAAPg9CAAMAAQACAAIAAAAUQIAAAAA+D0=";
+    
+    //base64 decode
+    //let buf_decode = 
+    let mut buf_decode = vec![0; BASE64.decode_len(data.len()).unwrap()];
+    let len = BASE64.decode_mut(data, &mut buf_decode).unwrap();
+    
+    // Unwrap ClassificationTop
+    let detections = flatbuffers::root::<ClassificationTop>(&buf_decode).unwrap();
+
+    // Get perception
+    let perception = detections.perception();
+
+    // Get length: ClassificationListLength
+
+
+    // Loop through ClassificationList, get classid and score
+    //for i in 0..5 {
+        //let classification = ???;
+    //}
+}
+
 fn main() {
-    //monster();
+    monster();
+    //monster2();
     //user();
     //classification();
-    test_base64_decoding();
+    //test_base64_decoding();
+    //test_deserialize_classification();
 }
